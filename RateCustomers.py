191383ae9fe2f512eeef1af4d4de68e71027c660
Jac Thomas <jacthomas@Jacs-MacBook-Pro-2.local>
2019-09-23 15:01:20 +0100

    reads to db, writes to file, prints top 5

--- RateCustomers.py
+++ RateCustomers.py
@@ -1,5 +1,6 @@
 import sqlite3 as db
 import csv
+import sys
 
 def create_tables():
 
@@ -11,6 +12,7 @@ def create_tables():
     cur.execute("drop table if exists orders")
 
     cur.execute("create table barcodes (barcode, order_id)")
+
     with open('barcodes.csv', 'r') as fin:  # `with` statement available in 2.5+
         # csv.DictReader uses first line in file for column headings by default
         dr = csv.DictReader(fin)  # comma is default delimiter
@@ -29,22 +31,84 @@ def create_tables():
 
     conn.commit()
     conn.close()
-    return db
+
+'''Refactor into database class'''
+def executeQuery(query):
+    conn = db.connect("Orders.db")
+    cur = conn.cursor()
+    cur.execute(query)
+    return cur.fetchall(), conn
+
+def executeNonQuery(query):
+    conn = db.connect("Orders.db")
+    cur = conn.cursor()
+    cur.execute(query)
+    conn.commit()
+    conn.close()
+
+def validate_barcodes():
+    '''validate no dupicate bar codes'''
+
+    dic, conn = executeQuery("Select count(barcode), barcode from barcodes group by barcode having count(barcode) > 1")
+
+    print(sys.stderr, "Deleting duplicate barcodes found for: "+", ".join(str(v) for k, v in dic))
+
+    executeNonQuery("delete from barcodes where barcode in"
+                "(select barcode from(Select count(barcode),barcode from barcodes "
+                "group by barcode having count(barcode) > 1))")
+
+def validate_orders():
+    '''validate no order without bar code'''
+
+    res, conn = executeQuery("select order_id from orders o where not exists "
+                "(select 1 from barcodes b where b.order_id = o.order_id )")
+    conn.close()
+    if res is not None:
+        print(sys.stderr, "Deleting orders with no barcode: " + ", ".join(str(i[0]) for i in res))
+
+        executeNonQuery("delete from orders where order_id in("
+                    "select order_id from orders o where not exists "
+                    "(select 1 from barcodes b where b.order_id = o.order_id ))")
+
+def get_customer_to_order():
+    data, conn = executeQuery("select o.customer_id, o.order_id, group_concat(b.barcode, ', ') from orders o, barcodes b "
+                "where b.order_id = o.order_id "
+                "group by o.customer_id, o.order_id")
+    conn.close()
+    return data
+
+def write_to_csv(data):
+    with open('customers.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['customer_id', 'order_id', 'barcodes'])
+        writer.writerows(data)
+        f.close()
 
 
-def read_orders():
+def print_top_five():
     ''''''
+    res, conn = executeQuery("select o.customer_id, count(b.barcode) from orders o, barcodes b "
+                 "where b.order_id = o.order_id "
+                 "group by o.customer_id "
+                 "order by count(b.barcode) desc")
+    print("Top customers: "+ ", ".join(str(res[i][0]) for i in range(0, 5)))
+    conn.close()
 
 
-def read_barcodes():
-    ''''''
 
 
 def main():
-    '''read orders'''
-    '''read barcodes'''
-    '''validate no order without bar code'''
-    '''validate no dupicate bar codes'''
+    create_tables()
+    validate_barcodes()
+    validate_orders()
+    dataset = get_customer_to_order()
+    write_to_csv(dataset)
+    print_top_five()
+
+
+
+
+
     '''join order to barcodes on order_id'''
     '''generate to output file'''
     '''group by customer amount of tickets, print top 5'''
--- RateCustomersTest.py
+++ RateCustomersTest.py
@@ -16,15 +16,63 @@ def test_create_tables():
     print(cur.fetchall())
     conn.close()
 
+
+def test_validate_barcodes():
+    db = create_tables()
+
     conn = db.connect("Orders.db")
     cur = conn.cursor()
-    cur.execute("drop table barcodes")
-    cur.execute("drop table orders")
+    cur.execute(
+        "select sum(c) from(Select count(barcode) as c, barcode from barcodes group by barcode having count(barcode) > 1)")
+
+    dupe_num = cur.fetchone()[0]
+
+    cur.execute("select count(barcode) from barcodes")
+    all_num = cur.fetchone()[0]
+    validate_barcodes(db)
+
+    conn = db.connect("Orders.db")
+    cur = conn.cursor()
+    cur.execute("select count(barcode) from barcodes")
+    all_num_after = cur.fetchone()[0]
+
+    assert all_num_after == all_num - dupe_num
+
+    cur.execute(
+        "select sum(c) from(Select count(barcode) as c, barcode from barcodes group by barcode having count(barcode) > 1)")
+    dupe_num_after = cur.fetchone()
+    assert dupe_num_after[0] is None
+
+
+def test_validate_orders():
+    db = create_tables()
+    conn = db.connect("Orders.db")
+    cur = conn.cursor()
+    cur.execute("select count(order_id) from orders o where not exists "
+                "(select 1 from barcodes b where b.order_id = o.order_id )")
+    num_no_barcode = cur.fetchone()[0]
+    cur.execute("select count(order_id) from orders")
+    total_num = cur.fetchone()[0]
     conn.close()
+
+    validate_orders(db)
+
+    conn = db.connect("Orders.db")
+    cur = conn.cursor()
+
+    cur.execute("select count(order_id) from orders")
+    total_num_after = cur.fetchone()[0]
+
+    assert total_num_after == total_num - num_no_barcode
+
+    cur.execute("select count(order_id) from orders o where not exists "
+                "(select 1 from barcodes b where b.order_id = o.order_id )")
+    num_no_barcode_after = cur.fetchone()[0]
+    assert num_no_barcode_after == 0
+
 
 
 if __name__ == "__main__":
     test_create_tables()
-
-
-
+    test_validate_barcodes()
+    test_validate_orders()
